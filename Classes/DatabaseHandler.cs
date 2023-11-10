c9d7527e3516d0a75fe57eee9ef66b781654c5a8
nullblank <nullblank@protonmail.com>
2023-11-10 16:26:22 +0800

    Added encrypted user handling

--- Classes/DatabaseHandler.cs
+++ Classes/DatabaseHandler.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Configuration;
-using System.Printing.IndexedProperties;
+using System.Data;
 using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace RC_IS.Classes
 {
@@ -15,11 +13,11 @@ namespace RC_IS.Classes
     {
         private readonly string connectionString;
         private MySqlConnection connection;
+
         public DatabaseHandler()
         {
             this.connectionString = ConfigurationManager.ConnectionStrings["MyDatabase"].ConnectionString;
             Trace.WriteLine("DATAINFO: " + this.connectionString);
-            OpenConnection();
         }
 
         private void OpenConnection()
@@ -46,7 +44,6 @@ namespace RC_IS.Classes
         public DataTable ExecuteQuery(string query)
         {
             OpenConnection();
-
             using (MySqlCommand command = new MySqlCommand(query, connection))
             {
                 using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
@@ -79,13 +76,91 @@ namespace RC_IS.Classes
         }
 
         // Other methods for specific database operations can be added here
-
         public void Dispose()
         {
             CloseConnection();
             connection?.Dispose();
         }
 
+        // AUTHENTICATE
+        public bool AuthenticateUser(string enteredUsername, string enteredPassword)
+        {
+            OpenConnection();
+            string query = "SELECT acc_name, acc_pass, acc_salt FROM tblacc WHERE acc_name = @Username";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Username", enteredUsername);
+                MySqlDataReader reader = command.ExecuteReader();
 
+                if (reader.Read())
+                {
+                    string accountName = reader["acc_name"].ToString();
+                    string accountPass = reader["acc_pass"].ToString();
+                    string salt = reader["acc_salt"].ToString();
+                    // Hasbrown the potato's password and add salt
+                    string enteredPasswordHash = HashPassword(enteredPassword, salt);
+                    // Is it the same hashbrown??
+                    return accountPass == enteredPasswordHash; // OPEN SAYS ME!
+                }
+            }
+
+            return false; // NONE SHALL PASS!
+        }
+
+        private string HashPassword(string password, string salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+                // Combine salt and password bytes
+                byte[] combinedBytes = saltBytes.Concat(passwordBytes).ToArray();
+                // Compute the hash
+                byte[] hashBytes = sha256.ComputeHash(combinedBytes);
+                // Convert the hash to a hexadecimal string
+                string hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                return hashString;
+            }
+        }
+        public void InsertUser(string username, string password)
+        {
+            // Generate a random salt
+            string salt = GenerateSalt();
+
+            // Hash the password with the generated salt
+            string hashedPassword = HashPassword(password, salt);
+
+            // Insert the user data into the database
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Implement your database insertion logic here
+                string query = "INSERT INTO tblacc (acc_name, acc_pass, acc_salt) VALUES (@Username, @Password, @Salt)";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    command.Parameters.AddWithValue("@Password", hashedPassword);
+                    command.Parameters.AddWithValue("@Salt", salt);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[32];
+            using (RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetBytes(saltBytes);
+            }
+
+            // Convert the salt to a base64-encoded string
+            string salt = Convert.ToBase64String(saltBytes);
+
+            return salt;
+        }
     }
 }
---
+++ Classes/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RC_IS.Classes
+{
+    internal class User
+    {
+        public string AccountName { get; set; }
+        public string AccountPass { get; set; }
+        public string AccountRole { get; set; }
+        public User(string accountname, string accountpass, string accountrole)
+        {
+            AccountName = accountname;
+            AccountPass = accountpass;
+            AccountRole = accountrole;
+        }
+        public override string ToString()
+        {
+            return $"{AccountName} {AccountPass} {AccountRole}";
+        }
+    }
+}
